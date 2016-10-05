895eabed0dfc62e540077a4b9149eafd88bf28d0
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-05 13:04:24 -0300

    #2 <@irlatrevisan, @marlonfurtado> - Criado user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, stretches: 20
 end
