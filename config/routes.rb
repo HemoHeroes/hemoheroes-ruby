521a5c0a5a0f377f39cc7c739bb2fbd10490479d
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-05 11:17:47 -0300

    #[2] <@irlatrevisan, @marlonfurtado> - Configurando rota root 'welcome'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
   devise_for :users
 
   resources :addresses
   resources :hospital_necessities
   resources :hospitals
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-  root 'welcome#index'
 
   get '/necessidadehospital', to: 'hospital_necessities#index'
 
