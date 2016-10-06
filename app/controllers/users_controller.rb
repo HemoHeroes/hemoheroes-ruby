5de25cdc54aef1bad609610d9b6750bee021d38f
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-06 13:24:42 -0300

    #2 <@marlonfurtado, @callegas> - Comentario excluido

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
     user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
 
-      end
+  end
 
 end
