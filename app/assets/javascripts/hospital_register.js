edbb1cc3bb473a155f85e2a3db595dcc3febb473
irlatrevisan <irlatrevisan@gmail.com>
2016-10-06 10:58:26 -0300

    #[1]<@mvoliveira, @lucasgaspari> - mudanças no form e nos botoes da tela de cadastro

---
+++ app/assets/javascripts/hospital_register.js
@@ -0,0 +1,12 @@
+
+<script language="JavaScript" >
+function enviardados(){
+
+if(document.:.tx_nome.value=="" || document.dados.tx_nome.value.length < 8)
+{
+alert( "Preencha campo NOME corretamente!" );
+document.dados.tx_nome.focus();
+return false;
+}
+
+</script>
