27c59499ac9884a6dd90ec623599884a19c53564
mvoliveira <cristianferreira_gks@hotmail>
2016-10-10 14:34:31 -0300

    #1 <@cristianferreira, @lucasgaspari> - Adicionar validacoes ao form com javascript

--- app/assets/javascripts/hospital_register.js
+++ app/assets/javascripts/hospital_register.js
@@ -9,9 +9,50 @@ ready(function(){
 
     var validateForm = function(){
       var inputName = document.getElementsByClassName('js-validateName')[0];
+      var inputEmail = document.getElementsByClassName('js-validateEmail')[0];
+      var inputCNPJ = document.getElementsByClassName('js-validateCNPJ')[0];
+      var inputCEP = document.getElementsByClassName('js-validateCEP')[0];
+      var inputDistrict = document.getElementsByClassName('js-validateDistrict')[0];
+      var inputStreet = document.getElementsByClassName('js-validateStreet')[0];
+      var inputCity = document.getElementsByClassName('js-validateCity')[0];
+      var inputNumber = document.getElementsByClassName('js-validateNumber')[0];
+      var inputState = document.getElementsByClassName('js-validateState')[0];
 
-      inputName.addEventListener('onchange', function(){
+
+      inputName.addEventListener('keyup', function(){
         validateFormService.validateName('js-validateName');
+      })
+
+      inputEmail.addEventListener('keyup', function(){
+        validateFormService.validateEmail('js-validateEmail');
+      })
+
+      inputCNPJ.addEventListener('keyup', function(){
+        validateFormService.validateCNPJ('js-validateCNPJ');
+      })
+
+      inputCEP.addEventListener('keyup', function(){
+        validateFormService.validateCEP('js-validateCEP');
+      })
+
+      inputDistrict.addEventListener('keyup', function(){
+        validateFormService.validateDistrict('js-validateDistrict');
+      })
+
+      inputStreet.addEventListener('keyup', function(){
+        validateFormService.validateStreet('js-validateStreet');
+      })
+
+      inputNumber.addEventListener('keyup', function(){
+        validateFormService.validateNumber('js-validateNumber');
+      })
+
+      inputCity.addEventListener('keyup', function(){
+        validateFormService.validateCity('js-validateCity');
+      })
+
+      inputState.addEventListener('keyup', function(){
+        validateFormService.validateState('js-validateState');
       })
 
     };
--- app/assets/javascripts/services/validate_form_service.js
+++ app/assets/javascripts/services/validate_form_service.js
@@ -5,15 +5,109 @@ var validateFormService = (function(){
     validateName: function(selector){
       var valueName = document.getElementsByClassName(selector)[0].value;
       console.log(valueName);
-
+      
       if (valueName == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+
+    validateEmail: function(selector){
+      var valueEmail = document.getElementsByClassName(selector)[0].value;
+      console.log(valueEmail);
+
+      if (valueEmail == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+
+
+    validateCNPJ: function(selector){
+      var valueCNPJ = document.getElementsByClassName(selector)[0].value;
+      console.log(valueCNPJ);
+
+      if (valueCNPJ == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+
+    validateCEP: function(selector){
+      var valueCEP = document.getElementsByClassName(selector)[0].value;
+      console.log(valueCEP);
+
+      if (valueCEP == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+    validateDistrict: function(selector){
+      var valueDistrict = document.getElementsByClassName(selector)[0].value;
+      console.log(valueDistrict);
+
+      if (valueDistrict == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+
+    validateStreet: function(selector){
+      var valueStreet = document.getElementsByClassName(selector)[0].value;
+      console.log(valueStreet);
+
+      if (valueStreet == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+
+    validateNumber: function(selector){
+      var valueNumber = document.getElementsByClassName(selector)[0].value;
+      console.log(valueNumber);
+
+      if (valueNumber == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+    validateCity: function(selector){
+      var valueCity = document.getElementsByClassName(selector)[0].value;
+      console.log(valueCity);
+
+      if (valueCity == "") {
+        console.log("chegou aqui")
+        alert('Preencha o campo com seu nome');
+        return false;
+      }
+    },
+
+    validateState: function(selector){
+      var valueState = document.getElementsByClassName(selector)[0].value;
+      console.log(valueState);
+
+      if (valueState == "") {
         console.log("chegou aqui")
         alert('Preencha o campo com seu nome');
         return false;
       }
     }
 
-    
+
   }
 })()
 
