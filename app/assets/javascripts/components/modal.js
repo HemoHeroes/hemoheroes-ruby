9fda5e7f914eeebd8090119a82cd5189bccf179e
shemoreira <sheilanara.web@hotmail.com>
2016-10-10 13:10:39 -0300

    #3 <@souzabruna, @lucimarNeves> - Alterado css

--- app/assets/javascripts/components/modal.js
+++ app/assets/javascripts/components/modal.js
@@ -2,12 +2,12 @@ var Modal = function(){
 
   return {
     open: function(){
-      const modal = document.getElementById('modal');
+      const modal = document.getElementsByClassName('js-modal')[0];
       modal.style.display = "block";
     },
 
     close: function(){
-      const modal = document.getElementById('modal');
+      const modal = document.getElementsByClassName('js-modal')[0];
       modal.style.display = "none";
     }
   }
