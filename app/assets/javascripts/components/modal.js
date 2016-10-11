3482dd1c44658168a385bf76ed2c5516c1d9175b
cschallenberger <brunaelisangelasouza@gmail.com>
2016-10-11 15:50:12 -0300

    #3 <@irlatrevisan, @souzabruna> - Alterar css e javascript da modal

--- app/assets/javascripts/components/modal.js
+++ app/assets/javascripts/components/modal.js
@@ -1,15 +1,24 @@
-var Modal = function(){
+var Modal = (function(){
 
   return {
     open: function(){
       const modal = document.getElementsByClassName('js-modal')[0];
-      modal.style.display = "block";
+      modal.style.opacity = 1;
+      modal.style.visibility = 'visible';
+
+      const modalContent = document.getElementsByClassName('js-modalContent')[0];
+      modalContent.style.top = '50%'
     },
+
 
     close: function(){
       const modal = document.getElementsByClassName('js-modal')[0];
-      modal.style.display = "none";
+      modal.style.opacity = 0;
+      modal.style.visibility = 'hidden';
+
+      const modalContent = document.getElementsByClassName('js-modalContent')[0];
+      modalContent.style.top = '40%'
     }
   }
 
-}();
+})()
