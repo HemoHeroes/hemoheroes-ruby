46c133fc4e7383e26047c77211a808bb722e6456
mathiasVoelcker <mathias_V94@hotmail.com>
2016-10-13 14:30:42 -0300

    #0 <@mathiasVoelcker, @cristianferreira, @lrsouza, @lucimarneves> Refatorar controller view e model

--- db/migrate/20161013142012_rename_donators_to_user_blood_donator.rb
+++
@@ -1,5 +0,0 @@
-class RenameDonatorsToUserBloodDonator < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :donators, :user_blood_donators
-  end
-end
--- db/migrate/20161013142253_rename_hospital_to_user_blood_bank.rb
+++
@@ -1,5 +0,0 @@
-class RenameHospitalToUserBloodBank < ActiveRecord::Migration[5.0]
-  def change
-      rename_table :hospitals, :user_blood_banks
-  end
-end
