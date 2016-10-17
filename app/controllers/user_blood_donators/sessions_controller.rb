c0bfdee9644e592597ab8448ca3f40f42b4a8b18
shemoreira <leonarrangel@gmail.com>
2016-10-17 11:57:46 -0200

    #2 <@lrsousa, @lucasgaspari> - Inserir boas vindas para usuario logado

--- app/controllers/user_blood_donators/sessions_controller.rb
+++ app/controllers/user_blood_donators/sessions_controller.rb
@@ -8,9 +8,7 @@ class UserBloodDonators::SessionsController < Devise::SessionsController
 
     # POST /resource/sign_in
     def create
-        puts "OPAAAAAAAAAAAAAAAAAAA"
         puts params.inspect
-        puts "OPAAAAAAAAAAAAAAAAAAAAAA"
         super
     end
 
