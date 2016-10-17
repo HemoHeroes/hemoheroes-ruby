7a9d6d49d910a0724437deb8d3fc86ccbc52a499
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-17 16:31:52 -0200

    #5 <@marlonfurtado, @lucimaNeves> -[DOING] Alterar form para validar button

--- app/assets/javascripts/donator_register.js
+++ app/assets/javascripts/donator_register.js
@@ -1,70 +1,30 @@
 //= require services/validate_form_donator
 
 ready(function(){
-  //REVER CONTROLLER (REGISTRATIONS???)
   onlyInView("registrations", ["new", "edit"], function(){
 
-
-
     var initialize = function(){
-      validateForm();
+      buttonValidFormDonator();
+      validateFormDonator();
     };
 
-    var validateForm = function(){
+    var buttonValidFormDonator = function(){
+      var buttonRegister = document.getElementsByClassName('js-validateButton')[0];
+      buttonRegister.classList.add('is-disabled');
+    }
 
-      var inputEmail            = document.getElementsByClassName('js-validateEmail')[0];
-      var inputCPF              = document.getElementsByClassName('js-validateCPF')[0];
-      var inputPhone            = document.getElementsByClassName('js-validatePhone')[0];
-      //var inputDate             = document.getElementsByClassName('js-validateDate')[0];
-      var radioGenre            = document.getElementsByClassName('js-validateGenre')[0];
-      var checkTerms            = document.getElementsByClassName('js-validateTerms')[0];
-      var buttonRegister        = document.getElementsByClassName('js-validateForm')[0];
-
-      buttonRegister.addEventListener('onclick', function(event){
-        validateFormDonators.validateName('js-validateName');
-        validateFormDonators.validateEmail('js-validateEmail');
-        validateFormDonators.validatePhone('js-validatePhone');
-        validateFormDonators.validateCPF('js-validateCPF');
-        //validateFormDonators.validateDate('js-validateDate');
-        validateFormDonators.validateTerms('js-validateTerms');
-        validateFormDonators.validateGenre('js-validateGenre');
-       event.preventDefault();
-      })
-
-
-        validateFormDonators.validateName('js-validateName','keyup');
-
-
-      inputEmail.addEventListener('keyup', function(){
-        validateFormDonators.validateEmail('js-validateEmail');
-      })
-
-      inputCPF.addEventListener('keyup', function(){
-        validateFormDonators.validateCPF('js-validateCPF');
-      })
-
-      inputPhone.addEventListener('keyup', function(){
-        validateFormDonators.validatePhone('js-validatePhone');
-      })
-
-      // inputDate.addEventListener('keyup', function(){
-      //   validateFormDonators.validateDate('js-validateDate');
-      // })
-      //
-
-
-      radioGenre.addEventListener('onclick', function(){
-        validateFormDonators.validateGenre('js-validateGenre');
-      })
-
-      checkTerms.addEventListener('checked', function(){
-        validateFormDonators.validateTerms('js-validateTerms');
-      })
-
+    var validateFormDonator = function(){
+      validateFormDonators.validateName('js-validateName', 'keyup');
+      validateFormDonators.validateEmail('js-validateEmail', 'keyup');
+      validateFormDonators.validatePhone('js-validatePhone', 'keyup');
+      validateFormDonators.validateCPF('js-validateCPF', 'keyup');
+      validateFormDonators.validateTerms('js-validateTerms', 'keyup');
+      validateFormDonators.validateGenre('js-validateGenre', 'keyup');
     };
+
 
     initialize();
 
-  })
+  });
 
-})
+});
--- app/assets/javascripts/services/validate_form_donator.js
+++ app/assets/javascripts/services/validate_form_donator.js
@@ -1,133 +1,190 @@
 var validateFormDonators = (function(){
 
+  var validFormDonator = [];
+  validFormDonator[0] = false;
+  validFormDonator[1] = false;
+  validFormDonator[2] = false;
+  validFormDonator[3] = false;
+  validFormDonator[4] = false;
+  validFormDonator[5] = false;
+  validFormDonator[6] = false;
+
+  var invalidInput = false;
+
+  var buttonValidFormDonator = function(){
+
+    invalidInput = false;
+    for(var i = 0; i < validFormDonator.length; i++){
+      console.log(validFormDonator[i])
+      if(validFormDonator[i]==false){
+        invalidInput = true;
+      }
+    }
+
+    if(!invalidInput){
+      var buttonRegister = document.getElementsByClassName('js-validateButton')[0];
+      buttonRegister.classList.remove('is-disabled');
+      buttonRegister.classList.add('is-actived');
+    }else{
+      var buttonRegister = document.getElementsByClassName('js-validateButton')[0];
+      buttonRegister.classList.remove('is-actived');
+      buttonRegister.classList.add('is-disabled');
+    }
+  }
+
   return {
 
-    validateName: function(selector){
+    validateName: function(selector, action){
+
       var valueName = document.getElementsByClassName(selector)[0];
-      document.getElementById('errorName').style.display = "";
-
-      if (valueName == "") {
-        errorName.style.display = "O campo é obrigatório!";
-        return false;
-      }
-      else {
+      valueName.addEventListener(action, function(){
         document.getElementById('errorName').style.display = "none";
-      }
+
+        if (valueName.value == "") {
+          document.getElementById('errorName').style.display = "O campo é obrigatório";
+          validFormDonator[0] = false;
+          buttonValidFormDonator();
+        }else{
+          validFormDonator[0] = true;
+          buttonValidFormDonator();
+        }
+      });
     },
 
 
-    validateEmail: function(selector){
-      var valueEmail = document.getElementsByClassName(selector)[0].value;
-      var regexEmailValidate = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      var emailValidate = regexEmailValidate.test(valueEmail.trim());
+    validateEmail: function(selector, action){
+      var valueEmail = document.getElementsByClassName(selector)[0];
+      valueEmail.addEventListener(action, function(){
+        document.getElementById('errorEmail').style.display = "";
+        var regexEmailValidate = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var emailValidate = regexEmailValidate.test(valueEmail.trim());
 
-      document.getElementById('errorEmail').style.display = "";
-      if (!emailValidate && valueEmail != "") {
-        document.getElementById('errorEmail').innerHTML = "Email inválido!";
-        return false;
-      }
-      if(valueEmail == ""){
-        document.getElementById('errorEmail').innerHTML = "O campo é obrigatório!";
-        return false;
-      }else{
-        document.getElementById('errorEmail').style.display = "none";
-      }
+        if (!emailValidate && valueEmail.value != "") {
+          document.getElementById('errorEmail').innerHTML = "Email inválido!";
+          validFormDonator[1] = false;
+          buttonValidFormDonator();
+          return false;
+        }
+        if(valueEmail.value == ""){
+          document.getElementById('errorEmail').innerHTML = "O campo é obrigatório!";
+          validFormDonator[1] = false;
+          buttonValidFormDonator();
+          return false;
+        }else{
+          document.getElementById('errorEmail').style.display = "none";
+          validFormDonator[1] = true;
+          buttonValidFormDonator();
+        }
+      });
     },
 
-    validateCPF: function(selector){
+
+    validateCPF: function(selector, action){
       var valueCPF = document.getElementsByClassName(selector)[0];
-      VMasker(valueCPF).maskPattern("999.999.999-99");
-      document.getElementById('errorCPF').style.display = "";
-      if(valueCPF.value == ""){
-        document.getElementById('errorCPF').innerHTML = "O campo é obrigatório!";
-        return false;
-      }else if (valueCPF.value.length<14) {
-        document.getElementById('errorCPF').innerHTML = "CPF inválido!";
-        return false;
-      }else{
-        document.getElementById('errorCPF').style.display = "none";
-      }
+      valueCPF.addEventListener(action, function(){
+
+        VMasker(valueCPF).maskPattern("999.999.999-99");
+        document.getElementById('errorCPF').style.display = "";
+        if(valueCPF.value == ""){
+          document.getElementById('errorCPF').innerHTML = "O campo é obrigatório!";
+          validFormDonator[2] = false;
+          buttonValidFormDonator();
+          return false;
+        }else if (valueCPF.value.length<14) {
+          document.getElementById('errorCPF').innerHTML = "CPF inválido!";
+          validFormDonator[2] = false;
+          buttonValidFormDonator();
+          return false;
+        }else{
+          document.getElementById('errorCPF').style.display = "none";
+          validFormDonator[2] = true;
+          buttonValidFormDonator();
+        }
+      });
     },
 
 
-    validatePhone: function(selector){
+    validatePhone: function(selector, action){
       var valuePhone = document.getElementsByClassName(selector)[0];
-      VMasker(valuePhone).maskPattern("(99)99999-9999");
-      document.getElementById('errorPhone').style.display = "";
-      if(valuePhone.value == ""){
-        document.getElementById('errorPhone').innerHTML = "O campo é obrigatório!";
-        return false;
-      }else if (valuePhone.value.length<14) {
-        document.getElementById('errorPhone').innerHTML = "Telefone inválido!";
-        return false;
-      }else{
-        document.getElementById('errorPhone').style.display = "none";
-      }
+      valuePhone.addEventListener(action, function(){
+
+        VMasker(valuePhone).maskPattern("(99)9999-9999");
+        document.getElementById('errorPhone').style.display = "";
+        if(valuePhone.value == ""){
+          document.getElementById('errorPhone').innerHTML = "O campo é obrigatório!";
+          validFormDonator[3] = false;
+          buttonValidFormDonator();
+          return false;
+        }else if (valuePhone.value.length<14) {
+          document.getElementById('errorPhone').innerHTML = "Telefone inválido!";
+          validFormDonator[3] = false;
+          buttonValidFormDonator();
+          return false;
+        }else{
+          document.getElementById('errorPhone').style.display = "none";
+          validFormDonator[3] = true;
+          buttonValidFormDonator();
+        }
+      });
     },
 
-    validateDate: function(selector){
+    validateDate: function(selector, action){
       var valueDate = document.getElementsByClassName(selector)[0];
-
-      // VMasker(valueDate).maskPattern("99/99/9999");
-      document.getElementById('errorDate').style.display = "";
-      if(valueDate.value == ""){
-        document.getElementById('errorDate').innerHTML = "O campo é obrigatório!";
-        return false;
-      }
-
-      else if (valueDate.value.length >= 7 && valueDate.value.length <= 10) {
-        document.getElementById('errorDate').innerHTML = "Data inválida!";
-        //console.log("outra coisa");
-        return false;
-      }
-
-      else {
-        document.getElementById('errorDate').style.display = "none";
-      }
+      valueDate.addEventListener(action, function(){
+        //  VMasker(valueDate).maskPattern("99/99/9999");
+        document.getElementById('errorDate').style.display = "";
+        if(valueDate.value == ""){
+          document.getElementById('errorDate').innerHTML = "O campo é obrigatório!";
+          validFormDonator[4] = false;
+          buttonValidFormDonator();
+          return false;
+        }else {
+          document.getElementById('errorDate').style.display = "none";
+          validFormDonator[4] = true;
+          buttonValidFormDonator();
+        }
+      });
     },
 
-    // validateDateLastDonation: function(selector){
-    //   var valueDateLastDonation = document.getElementsByClassName(selector)[0];
-    //   VMasker(valueDateLastDonation).maskPattern("99/99/9999");
-    //   document.getElementById('errorDateLastDonation').style.display = "";
-    //   if(valueDateLastDonation.value == ""){
-    //     document.getElementById('errorDateLastDonation').innerHTML = "O campo é obrigatório!";
-    //     return false;
-    //   }else if (valueDateLastDonation.value.length<10) {
-    //     document.getElementById('errorDateLastDonation').innerHTML = "Data inválida!";
-    //     console.log("Data inválida");
-    //     return false;
-    //   }else {
-    //     document.getElementById('errorDateLastDonation').style.display = "none";
-    //   }
-    // },
-
-    validateGenre: function(selector){
+    validateGenre: function(selector, action){
       var valueGenre = document.getElementsByClassName(selector)[0];
-      document.getElementById('errorGenre').style.display = "";
+      valueGenre.addEventListener(action, function(){
 
-      if(valueGenre.checked) {
-        document.getElementById('errorGenre').style.display = "none";
-      }
-      else {
-        document.getElementById('errorGenre').innerHTML = "O campo é obrigatório!";
-        return false;
-      }
+        document.getElementById('errorGenre').style.display = "";
+        if(valueGenre.checked) {
+          document.getElementById('errorGenre').style.display = "none";
+          validFormDonator[5] = true;
+          buttonValidFormDonator();
+        }
+        else {
+          document.getElementById('errorGenre').innerHTML = "O campo é obrigatório!";
+          validFormDonator[5] = false;
+          buttonValidFormDonator();
+          return false;
+        }
+      });
     },
 
     validateTerms: function(selector){
       var valueTerms = document.getElementsByClassName(selector)[0];
-      document.getElementById('errorTerms').style.display = "";
+      valueTerms.addEventListener(action, function(){
 
-      if(valueTerms.checked) {
-        document.getElementById('errorTerms').style.display = "none";
-      }
-      else {
-        document.getElementById('errorTerms').innerHTML = "O campo é obrigatório!";
-        return false;
-      }
+        document.getElementById('errorTerms').style.display = "";
+        if(valueTerms.checked) {
+          document.getElementById('errorTerms').style.display = "none";
+          validFormDonator[6] = true;
+          buttonValidFormDonator();
+        }
+        else {
+          document.getElementById('errorTerms').innerHTML = "O campo é obrigatório!";
+          validFormDonator[6] = false;
+          buttonValidFormDonator();
+          return false;
+        }
+      });
     }
-  }
 
+
+  } // return
 
 })()
