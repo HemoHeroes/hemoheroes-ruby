e253d7883a0e8b8c468bfa4b89548ecd4380586a
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-18 13:13:10 -0200

    #5 <@callegas, @marlonfurtado> - Validar senha

--- app/assets/javascripts/donator_register.js
+++ app/assets/javascripts/donator_register.js
@@ -1,7 +1,7 @@
 //= require services/validate_form_donator
 
 ready(function(){
-  // onlyInView("registrations", ["new", "edit"], function(){
+   //onlyInView("registrations", ["new", "edit"], function(){
 
     var initialize = function(){
       buttonValidFormDonator();
@@ -11,20 +11,20 @@ ready(function(){
     var buttonValidFormDonator = function(){
       var buttonRegister = document.getElementsByClassName('js-validateButton')[0];
       buttonRegister.classList.add('is-disabled');
-      //buttonRegister.classList.add('is-actived');
     }
 
     var validateFormDonator = function(){
       validateFormDonators.validateName('js-validateName', 'keyup');
+      validateFormDonators.validateCPF('js-validateCPF', 'keyup');
       validateFormDonators.validateEmail('js-validateEmail', 'keyup');
       validateFormDonators.validatePhone('js-validatePhone', 'keyup');
-      validateFormDonators.validateCPF('js-validateCPF', 'keyup');
       validateFormDonators.validateTerms('js-validateTerms', 'click');
+      validateFormDonators.validatePassword('js-validatePassword','js-validatePasswordConfirmation','keyup');
     };
 
 
     initialize();
 
-  });
+  //});
 
-// });
+ });
--- app/assets/javascripts/services/validate_form_donator.js
+++ app/assets/javascripts/services/validate_form_donator.js
@@ -6,6 +6,8 @@ var validateFormDonators = (function(){
   validFormDonator[2] = false;
   validFormDonator[3] = false;
   validFormDonator[4] = false;
+  validFormDonator[5] = false;
+  validFormDonator[6] = false;
 
   var invalidInput = false;
 
@@ -145,7 +147,51 @@ var validateFormDonators = (function(){
           return false;
         }
       });
-    }
+    },
+
+
+        validatePassword: function(selector, selectorConfirmation, action ){
+          var valuePassword = document.getElementsByClassName(selector)[0];
+          var valuePasswordConfirmation = document.getElementsByClassName(selectorConfirmation)[0];
+
+          valuePassword.addEventListener(action, function(){
+            document.getElementById('errorPassword').style.display = "";
+            if(valuePassword.value.length > 0 && valuePassword.value.length < 6){
+              document.getElementById('errorPassword').innerHTML = "Senha deve ter no minimo 6 digitos!";
+              validFormDonator[5] = false;
+              buttonValidFormDonator();
+              return false;
+            }else if(valuePassword.value.length == 0){
+              document.getElementById('errorPassword').innerHTML = "O campo senha é obrigatório!";
+              validFormDonator[5] = false;
+              buttonValidFormDonator();
+              return false;
+            }else{
+              document.getElementById('errorPassword').style.display = "none";
+              validFormDonator[5] = true;
+              buttonValidFormDonator();
+            }
+          });
+
+          valuePasswordConfirmation.addEventListener(action, function(){
+            document.getElementById('errorPasswordConfirmation').style.display = "";
+
+            if(valuePasswordConfirmation.value.length >= 0){
+              if(valuePasswordConfirmation.value != valuePassword.value){
+                document.getElementById('errorPasswordConfirmation').innerHTML = "Senhas não correspondem!";
+                validFormDonator[6] = false;
+                buttonValidFormDonator();
+              }else{
+                document.getElementById('errorPasswordConfirmation').style.display = "none";
+                validFormDonator[6] = true;
+                buttonValidFormDonator();
+              }
+            }
+
+          });
+
+        }
+
 
 
   } // return
