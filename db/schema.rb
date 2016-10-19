c28292a1fe284ce8ae65cba08d0800a45f5ffcca
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-10-19 15:46:53 -0200

    #0 <@mathiasVoelcker, @marlonfurtado> - adaptar codigo para variavel ENV_FOR_HOTJAR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161019164529) do
 
   create_table "demand_blood_banks", force: :cascade do |t|
