08f3aefe9b27654f2aada27a5ff5c8ff1b962423
irlatrevisan <leonarrangel@gmail.com>
2016-10-21 10:31:44 -0200

    #1 <@cschallenberger, @lrsousa> - Testar se hospital com acesso liberado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,12 +18,17 @@ class SessionsController < Devise::SessionsController
     if (@user == nil)
       redirect_to root_path , alert:"Documento inválido", flash: { manifesto_modal: true }
     else
+      unless @user.actived
+        return redirect_to root_path , alert:"Usuário ainda não autorizado!", flash: { manifesto_modal: true }
+      end
+
       if @user.valid_password?(senha)
         sign_in(@user)
         user_dashboard(@user)
       else
         redirect_to root_path , alert:"Senha inválida", flash: { manifesto_modal: true }
       end
+
     end
   end
 
