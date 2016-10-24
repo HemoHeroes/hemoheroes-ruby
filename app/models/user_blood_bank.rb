fdeb0e5fc95600a4cc51aab0ad6d38f9df642a6a
irlatrevisan <leonarrangel@gmail.com>
2016-10-24 10:58:29 -0200

    #5 <@cschallenberger, @lrsousa> - inserir validacao de atributos em model

--- app/models/user_blood_bank.rb
+++ app/models/user_blood_bank.rb
@@ -1,4 +1,8 @@
 class UserBloodBank < ApplicationRecord
+  validates_presence_of :name, :email, :cnpj, :phone, :extension, :address
+
+  validates_uniqueness_of :cnpj
+  validates_uniqueness_of :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
