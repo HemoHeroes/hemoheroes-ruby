908c7cb676cd2bda4ee446c7fcf61dc251d69aa8
irlatrevisan <leonarrangel@gmail.com>
2016-10-24 10:28:10 -0200

    #5 <@cschallenberger, @lrsousa> - Arrumar bug que quebrava o site quando cadastrava dois doadores com mesmo email

--- app/models/user_blood_donator.rb
+++ app/models/user_blood_donator.rb
@@ -1,5 +1,6 @@
 class UserBloodDonator < ApplicationRecord
   validates_uniqueness_of :cpf
+  validates_uniqueness_of :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
