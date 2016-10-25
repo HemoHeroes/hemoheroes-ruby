3120fe90807d063e55b5272b455bf8e142da572d
callegas <sheilanara.web@hotmail.com>
2016-10-25 14:58:46 -0200

    #2 <@shemoreira, @cshallenberger> - Logar com email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,20 +8,21 @@ class SessionsController < Devise::SessionsController
     @user = nil
     senha = get_param_password params
 
-    @user = UserBloodDonator.find_for_authentication(cpf: params[:document])
+    @user = UserBloodDonator.find_for_authentication(email: params[:document])
 
     if (@user == nil)
-      @user = UserBloodBank.find_for_authentication(cnpj: params[:document])
+      @user = UserBloodBank.find_for_authentication(email: params[:document])
     end
 
 
     if (@user == nil)
       redirect_to root_path , alert:"Documento inválido", flash: { manifesto_modal: true }
     else
-      unless @user.actived
-        return redirect_to root_path , alert:"Usuário ainda não autorizado!", flash: { manifesto_modal: true }
+      if @user.instance_of? UserBloodBank
+        unless @user.actived
+          return redirect_to root_path , alert:"Usuário ainda não autorizado!", flash: { manifesto_modal: true }
+        end
       end
-
       if @user.valid_password?(senha)
         sign_in(@user)
         user_dashboard(@user)
