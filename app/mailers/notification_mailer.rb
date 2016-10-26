1c06eb1f6e289806afa99ebb2b57d01a5b537d1e
mvoliveira <matheus.mvo10@gmail.com>
2016-10-26 15:39:13 -0200

    #1 <@mathiasVoelcker, @mvoliveira> - Alteração no corpo do email de notificação de novo banco de sangue.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,21 +11,22 @@ class NotificationMailer < ApplicationMailer
     template_name: 'welcome_email')
   end
 
-  def check_able_to_donate(donator)
-    difference_in_days = (DateTime.now.to_date - donator.last_donation).to_i
-    if difference_in_days > 30
-      return true
-    end
-    return false
-  end
 
-  def send_notification_to_admin(blood_bank)
+  def send_notification_to_admin blood_bank
     @url = 'http://hemoheroestw-staging.herokuapp.com/admin'
     @blood_bank_name = blood_bank.name
     mail(to: 'aceleradora10@gmail.com',
     subject: 'Validar cadastro de novo banco de sangue',
     template_path: 'notification_mailer',
     template_name: 'new_blood_bank_email')
+  end
+
+  def check_able_to_donate(donator)
+    difference_in_days = (DateTime.now.to_date - donator.last_donation).to_i
+    if difference_in_days > 30
+      return true
+    end
+    return false
   end
 
 end
