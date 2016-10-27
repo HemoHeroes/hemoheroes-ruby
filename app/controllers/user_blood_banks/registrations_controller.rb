c8a71f927c1085f748b375025d4bd497b0b95e44
lucimarNeves <lucimar.neves@gmail.com>
2016-10-27 11:34:05 -0200

    #1<@cristianferreira, @lucimarNeves> - Abrir modal cadastro hospital

--- app/controllers/user_blood_banks/registrations_controller.rb
+++ app/controllers/user_blood_banks/registrations_controller.rb
@@ -67,7 +67,7 @@ class UserBloodBanks::RegistrationsController < Devise::RegistrationsController
     # The path used after sign up.
     def after_sign_up_path_for(resource)
       sign_out
-      
+
 
     end
     #
