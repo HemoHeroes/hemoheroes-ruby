481bb2d0ced75eaabb8083573a6b6e9dc09b51ec
souzabruna <brunaelisangelasouza@gmail.com>
2016-11-07 13:50:22 -0200

    #5 <@souzabruna, @mathiasVoelcker, @lucimarNeves> - Implementar teste pagina de cadastro de doador

--- spec/features/register_blood_donator_spec.rb
+++ spec/features/register_blood_donator_spec.rb
@@ -26,6 +26,134 @@ describe "Antônio poderá criar uma conta de doador", type: :feature, js: true 
 
       expect(page).to have_button(class: "is-actived")
     end
-
   end
+
+  context "cadastro com email incorreto" do
+    it "deve manter o botao desabilitado" do
+      visit "/"
+      find(".Button").click
+      within("#form-registration-blood-donator") do
+        fill_in "user_blood_donator_name", with: "Lucimar"
+          page.find('#user_blood_donator_name').trigger(:focusout)
+
+        fill_in "user_blood_donator_email", with: "Lucimaraceleradora.com"
+          page.find('#user_blood_donator_email').trigger(:focusout)
+
+        fill_in "user_blood_donator_password", with: "Lucimar"
+          page.find('#user_blood_donator_password').trigger(:focusout)
+
+        fill_in "user_blood_donator_password_confirmation", with: "Lucimar"
+          page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
+
+        select("A+", from: "user_blood_donator_blood_type")
+        find_by_id("accept_terms").trigger('click')
+
+      end
+
+      expect(page).to have_button(class: "is-disabled")
+    end
+  end
+
+  context "cadastro com nome incorreto" do
+    it "deve manter o botao desabilitado" do
+      visit "/"
+      find(".Button").click
+      within("#form-registration-blood-donator") do
+        fill_in "user_blood_donator_name", with: ""
+          page.find('#user_blood_donator_name').trigger(:focusout)
+
+        fill_in "user_blood_donator_email", with: "Lucimar@aceleradora.com"
+          page.find('#user_blood_donator_email').trigger(:focusout)
+
+        fill_in "user_blood_donator_password", with: "Lucimar"
+          page.find('#user_blood_donator_password').trigger(:focusout)
+
+        fill_in "user_blood_donator_password_confirmation", with: "Lucimar"
+          page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
+
+        select("A+", from: "user_blood_donator_blood_type")
+        find_by_id("accept_terms").trigger('click')
+      end
+      expect(page).to have_button(class: "is-disabled")
+    end
+  end
+
+  context "cadastro com senhas diferentes" do
+    it "deve manter o botao desabilitado" do
+      visit "/"
+      find(".Button").click
+      within("#form-registration-blood-donator") do
+        fill_in "user_blood_donator_name", with: "Lucimar123"
+          page.find('#user_blood_donator_name').trigger(:focusout)
+
+        fill_in "user_blood_donator_email", with: "Lucimar@aceleradora.com"
+          page.find('#user_blood_donator_email').trigger(:focusout)
+
+        fill_in "user_blood_donator_password", with: "Lucimar"
+          page.find('#user_blood_donator_password').trigger(:focusout)
+
+        fill_in "user_blood_donator_password_confirmation", with: "Mathias"
+          page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
+
+        select("A+", from: "user_blood_donator_blood_type")
+        find_by_id("accept_terms").trigger('click')
+
+      end
+
+      expect(page).to have_content("Senhas não correspondem")
+      expect(page).to have_button(class: "is-disabled")
+    end
+  end
+
+  context "cadastro com senhas menor que 6 caracteres" do
+    it "deve manter o botao desabilitado" do
+      visit "/"
+      find(".Button").click
+      within("#form-registration-blood-donator") do
+        fill_in "user_blood_donator_name", with: "Lucimar123"
+          page.find('#user_blood_donator_name').trigger(:focusout)
+
+        fill_in "user_blood_donator_email", with: "Lucimar@aceleradora.com"
+          page.find('#user_blood_donator_email').trigger(:focusout)
+
+        fill_in "user_blood_donator_password", with: "Luci"
+          page.find('#user_blood_donator_password').trigger(:focusout)
+
+        fill_in "user_blood_donator_password_confirmation", with: "Luci"
+          page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
+
+        select("A+", from: "user_blood_donator_blood_type")
+        find_by_id("accept_terms").trigger('click')
+
+      end
+
+      expect(page).to have_content("Senha deve ter no minimo 6 digitos!")
+      expect(page).to have_button(class: "is-disabled")
+    end
+  end
+
+  context "cadastro com senhas menor que 6 caracteres" do
+    it "deve manter o botao desabilitado" do
+      visit "/"
+      find(".Button").click
+      within("#form-registration-blood-donator") do
+        fill_in "user_blood_donator_name", with: "Lucimar123"
+          page.find('#user_blood_donator_name').trigger(:focusout)
+
+        fill_in "user_blood_donator_email", with: "Lucimar@aceleradora.com"
+          page.find('#user_blood_donator_email').trigger(:focusout)
+
+        fill_in "user_blood_donator_password", with: "Lucimar"
+          page.find('#user_blood_donator_password').trigger(:focusout)
+
+        fill_in "user_blood_donator_password_confirmation", with: "Lucimar"
+          page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
+
+        select("A+", from: "user_blood_donator_blood_type")
+
+      end
+      expect(page).to have_button(class: "is-disabled")
+    end
+  end
+
 end
