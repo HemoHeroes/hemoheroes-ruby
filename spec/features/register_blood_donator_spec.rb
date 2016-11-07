877df4c96ed27399a2cdae25e668668645f6f9bf
shemoreira <sheilanara.web@hotmail.com>
2016-11-07 13:19:27 -0200

    #5 <@mathiasVoelcker, @lucimarneves, @souzabruna> - criar teste de caminho feliz para cadastro de doador

--- spec/features/register_blood_donator_spec.rb
+++ spec/features/register_blood_donator_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "Antônio poderá criar uma conta de doador", type: :feature, js: true do
   context "cadastro com dados corretos" do
 
-    it "deve abrir a modal" do
+    it "deve ativar botão de cadastro" do
       visit "/"
       find(".Button").click
       within("#form-registration-blood-donator") do
@@ -20,8 +20,7 @@ describe "Antônio poderá criar uma conta de doador", type: :feature, js: true 
           page.find('#user_blood_donator_password_confirmation').trigger(:focusout)
 
         select("A+", from: "user_blood_donator_blood_type")
-        check(".js-validateTerms")
-        # find(:css "accept_terms['1']").click()
+        find_by_id("accept_terms").trigger('click')
 
       end
 
