32efad44f741e1f0fd5e9b91e017c5c4405af3a2
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-11-08 15:30:10 -0200

    #5 <@marlonfurtado> - Retirar campo senha do form de cadastro

--- app/assets/javascripts/donator_register_prototype.js
+++ app/assets/javascripts/donator_register_prototype.js
@@ -1,24 +1,24 @@
 ready(function(){
 
   // onlyInView("user_blood_donators", ["newPrototipo", "edit"],"blood_prototype", function(){
-    var initialize = function(){
-      buttonValidSimpleFormDonator();
-      validateSimpleFormDonator();
-    };
+  var initialize = function(){
+    buttonValidSimpleFormDonator();
+    validateSimpleFormDonator();
+  };
 
-    var buttonValidSimpleFormDonator = function(){
-      var buttonRegisterSimpleDonator = document.querySelector('.js-validateButtonSimpleDonator');
-      buttonRegisterSimpleDonator.classList.add('is-disabled');
-    }
+  var buttonValidSimpleFormDonator = function(){
+    var buttonRegisterSimpleDonator = document.querySelector('.js-validateButtonSimpleDonator');
+    buttonRegisterSimpleDonator.classList.add('is-disabled');
+  }
 
-    var validateSimpleFormDonator = function(){
-      validateFormService.validateName('.js-validateName', 'focusout');
-      validateFormService.validateEmail('.js-validateEmail', 'focusout');
-      validateFormService.validateTerms('.js-validateTerms', 'click');
-      validateFormService.validatePassword('.js-validatePassword','.js-validatePasswordConfirmation','keyup');
-    };
+  var validateSimpleFormDonator = function(){
+    validateFormService.validateName('.js-validateName', 'focusout');
+    validateFormService.validateEmail('.js-validateEmail', 'focusout');
+    validateFormService.validateTerms('.js-validateTerms', 'click');
+    //validateFormService.validatePassword('.js-validatePassword','.js-validatePasswordConfirmation','keyup');
+  };
 
-    initialize();
+  initialize();
 
   // });
 
