cd9863b521c5619ca0307ab62556029df14cba23
mathiasVoelcker <mathias_V94@hotmail.com>
2016-11-09 10:56:51 -0200

    #3 <@mathiasVoelcker, @cristianferreira> - criar teste para inserir e apagar dados em form de solicitar doadores

--- spec/features/demand_blood_spec.rb
+++ spec/features/demand_blood_spec.rb
@@ -28,4 +28,21 @@ describe "Katia poderá solicitar doador.", type: :feature, js: true do
       expect(page).to have_css(".Button.Button--medium.Button--fluid.is-disabled.js-nextButton")
     end
   end
+
+  context "quando inserir e apagar dados" do
+    it "não deve ativar o botao" do
+      visit "/necessidadeBanco"
+
+      fill_in "demand_blood_bank_a_positive", with: "5"
+      page.find('#demand_blood_bank_a_positive').trigger(:focusout)
+      fill_in "demand_blood_bank_b_positive", with: "5"
+      page.find('#demand_blood_bank_a_positive').trigger(:focusout)
+      fill_in "demand_blood_bank_a_positive", with: "0"
+      page.find('#demand_blood_bank_a_positive').trigger(:focusout)
+      fill_in "demand_blood_bank_b_positive", with: ""
+      page.find('#demand_blood_bank_b_positive').trigger(:focusout)
+
+      expect(page).to have_css(".Button.Button--medium.Button--fluid.is-disabled.js-nextButton")
+    end
+  end
 end
