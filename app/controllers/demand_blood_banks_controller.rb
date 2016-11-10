1269ae36c3e2ba5aaffe825c5a25b5356020fa82
cristianferreira <cristianferreira_gks@hotmail.com>
2016-11-10 10:56:54 -0200

    #0 <@mathiasVoelcker, @cristianferreira> - resolver build

--- app/controllers/demand_blood_banks_controller.rb
+++ app/controllers/demand_blood_banks_controller.rb
@@ -16,7 +16,6 @@ class DemandBloodBanksController < ApplicationController
   # GET /demand_blood_banks/new
   def new
     @demand_blood_bank = DemandBloodBank.new
-    # @lastUpdateNecessity = DemandBloodBank.last.updated_at.strftime("%d/%m/%Y - %H:%M:%S")
     @lastUpdateNecessity = DemandBloodBank.last
   end
 
