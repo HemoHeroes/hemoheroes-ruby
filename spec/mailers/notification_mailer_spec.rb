3d7fa1881d7bf72b1e7ce7d1bd1acbb0f896184e
marlonfurtado <marlonfurtado.rs@gmail.com>
2016-11-10 09:51:15 -0200

    #4 <@mvoliveira, @marlonfurtado> - Refatorar testes.

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,27 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe NotificationMailer, type: :mailer do
+describe NotificationMailer, type: :mailer do
 
-  describe 'O sistema deverá enviar notificações' do
+  before(:all) do
+    @user = UserBloodDonator.new(name: 'Usuário De Teste', email: 'usuario@gmail.com', password: '123456', password_confirmation: '123456')
+    @mail = NotificationMailer.send_email_new_user(@user).deliver_now
+  end
 
-    before(:all) do
-      @user = UserBloodDonator.new(name: 'Usuário De Teste', email: 'aceleradora10@gmail.com', password: '123456', password_confirmation: '123456')
-      @mail = NotificationMailer.send_email_new_user(@user).deliver_now
+
+  describe '#send_email_new_user' do
+    it 'deve retornar o conteúdo do texto' do
+      expect(@mail.subject).to eq("Bem vindo ao HemoHeroes")
     end
 
-    context 'instructions' do
+    it 'deve retornar o email do destinatário' do
+      expect(@mail.to).to eq(["usuario@gmail.com"])
+    end
 
-      it 'Deve Receber email' do
-        expect(@mail.subject).to eq("Bem vindo ao HemoHeroes")
-      end
-
-      it 'renders the receiver email' do
-        expect(@mail.to).to eq(["aceleradora10@gmail.com"])
-      end
-
-      it 'renders the sender email' do
-        expect(@mail.from).to eq(["aceleradora10@gmail.com"])
-      end
+    it 'deve retornar o email do remetente ' do
+      expect(@mail.from).to eq(["aceleradora10@gmail.com"])
     end
   end
 end
