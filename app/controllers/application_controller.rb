4330826c8675862204a79ffdfcb0953d2b20c090
callegas <lucasgaspari1998@gmail.com>
2016-11-21 11:04:49 -0200

    #0 <@lucasgaspari, @lrsousa> - Refatorar query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
 
   def send_no_blood_type
-    @donators = UserBloodDonator.all
+    @donators = UserBloodDonator.where(blood_type:"", notification:true)
     @donators.each do |donator|
-      if((donator.blood_type == "") && check_donation_interval(donator) && donator.notification)
+      if(check_donation_interval(donator))
         NotificationMailer.send_email_no_blood_type_donator(donator).deliver_now
         Notification.create! :last_notification => Date.current,
         :appear => false,
