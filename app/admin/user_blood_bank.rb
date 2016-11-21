e0d4479f77924ed364c56524c40c343c5865df70
mathiasVoelcker <c.schallenberger@hotmail.com>
2016-11-21 10:41:32 -0200

    criar form em editar userBloodBank em admin

--- app/admin/user_blood_bank.rb
+++ app/admin/user_blood_bank.rb
@@ -9,6 +9,25 @@ ActiveAdmin.register UserBloodBank do
     actions
   end
 
+  form do |f|
+    f.label "Ativar Banco"
+    f.check_box :actived
+    f.actions do
+      f.action :submit
+      f.action :submit, label: "Create Featured Product", url: "some_url_for_create(@product)"
+      f.action :cancel, :wrapper_html => { :class => 'cancel'}
+    end
+    controller do
+     def update(options={}, &block)
+      # You can put your send email code over here
+      super do |success, failure|
+        block.call(success, failure) if block
+        failure.html { render :edit }
+      end
+     end
+  end
+  end
+
 
 permit_params :name, :email, :cnpj, :password, :phone, :lat, :long, :address, :extension, :actived
 
