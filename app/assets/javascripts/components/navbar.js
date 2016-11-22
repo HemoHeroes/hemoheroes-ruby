ed52c913f022c8a5aa18dfb940c2bd6cfeca7455
souzabruna <brunaelisangelasouza@gmail.com>
2016-11-22 11:04:37 -0200

    #14 <@souzabruna, @lucasgaspari> - Ajustar menu responsivo

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -1,10 +1,26 @@
 var Navbar = (function(){
 
   var initialize = function(){
-    // changeOnScroll();
-  };
+    ready(function(){
+      var currentController = document.body.getAttribute("data-controller");
+      var currentAction = document.body.getAttribute("data-action");
 
-  var navbar = document.querySelector('.HeroImage-header');
+      if (currentController == 'application' && currentAction == 'index') {
+        Navbar.toggle({
+          icon: '.js-openNavbar',
+          navbar: '.HeroImage-header',
+          links: '.js-navbarLinks'
+        });
+      } else {
+        Navbar.toggle({
+          icon: '.js-openNavbar',
+          navbar: '.Navbar',
+          links: '.js-navbarMobile'
+        });
+      }
+    });
+  }
+
 
   var verifyScrolled = function(scrolled){
     if (scrolled){
@@ -26,27 +42,31 @@ var Navbar = (function(){
     });
   };
 
-  var showNavbar = function(selector){
-    var navbarLinks = document.querySelector(selector);
-    var navbar = document.querySelector(".HeroImage-header");
 
-    if (navbarLinks.style.display == "none" || navbarLinks.style.display === "") {
-      navbar.classList.add('is-scrolling');
-      navbarLinks.style.display = "inline-block";
-    } else {
-      navbar.classList.remove('is-scrolling');
-      navbarLinks.style.display = "none";
-    }
+  var showNavbarLinks = function(params){
+    var iconToOpen = document.querySelector(params.icon);
+    var navbar = document.querySelector(params.navbar);
+    var links = document.querySelector(params.links);
+
+    iconToOpen.addEventListener('click', function(){
+      if (links.style.display == "none" || links.style.display === "") {
+        navbar.classList.add('is-scrolling');
+        links.style.display = "inline-block";
+      } else {
+        navbar.classList.remove('is-scrolling');
+        links.style.display = "none";
+      }
+    });
   };
 
 
   return {
 
-    toggle : function(selector){
-      showNavbar(selector);
+    toggle : function(params){
+      showNavbarLinks(params);
     },
 
-    initialize: initialize()
+    initialize : initialize()
 
   };
 
---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,22 @@
+// ready(function(){
+//
+//   onlyInView("application", ["index"],null, function(){
+//
+//     var initialize = function(){
+//       openMobileNavbar();
+//     };
+//
+//     var openMobileNavbar = function(){
+//       Navbar.toggle({
+//         icon: '.js-openNavbar',
+//         navbar: '.HeroImage-header',
+//         links: '.js-navbarLinks'
+//       });
+//     };
+//
+//     return {
+//       initialize: initialize()
+//     }
+//
+//   });
+// });
