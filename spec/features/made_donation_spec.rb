bd59890284ade4c307137edc555015fe97e1ceac
mathiasVoelcker <mathias_V94@hotmail.com>
2016-11-23 10:58:42 -0200

    #8 <@mathiasVoelcker> - criar teste para link Ja Doei

---
+++ spec/features/made_donation_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper.rb"
+
+describe "Antônio poderá informar que já dppi sangue" do
+
+  before(:all) do
+    UserBloodDonator.create! :name => 'Pedrao dos Login',
+      :email => 'cristianferreira_gks@hotmail.com',
+      :password => '123456', :password_confirmation => '123456',
+      :cpf => '12345678912', :blood_type => 'O-', :notification => true,
+      :genre => 'm'
+  end
+
+  context "Quando clicar em link Doei Ultimamente" do
+    it "deve aparecer modal de Ja Doei" do
+      user = UserBloodDonator.last
+      user.last_donation_token = 1
+      token = user.last_donation_token
+      url = "/madeDonation/" + token
+      visit url
+      expect(page).to have_css(".Modal.js-modal.js-modalSuccessMessage.Modal-overlay")
+      expect(page).to have_css(".Button.Button--feedback.u-margin-top-medium.is-actived")
+    end
+  end
+end
