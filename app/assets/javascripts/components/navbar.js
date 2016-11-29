563d2daa6faed87fe30c9200255b957062efd776
souzabruna <brunaelisangelasouza@gmail.com>
2016-11-29 10:59:55 -0200

    #0 <@souzabruna, @cschallenberger> - Adicionar flexbox no navbar

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -27,35 +27,16 @@ var Navbar = (function(){
     var navbar = document.querySelector(params.navbar);
     var links = document.querySelector(params.links);
 
-    links.addEventListener('click', function(){
-      if (links.style.display == "none" || links.style.display === "") {
-        navbar.classList.add('is-scrolling');
-        links.style.display = "inline-block";
-        console.log("if");
-      } else {
-        navbar.classList.remove('is-scrolling');
-        if (iconToOpen.style.display == "none") {
-          console.log("if2");
-          links.style.display = "inline";
-        } else {
-          console.log("else2");
-          links.style.display = "none";
-        }
-        console.log("else");
-      }
-    });
-
     iconToOpen.addEventListener('click', function(){
       if (links.style.display == "none" || links.style.display === "") {
-        console.log("abriu menu");
         navbar.classList.add('is-scrolling');
         links.style.display = "inline-block";
       } else {
-        console.log("fechou menu");
         navbar.classList.remove('is-scrolling');
         links.style.display = "none";
       }
     });
+
   };
 
 
